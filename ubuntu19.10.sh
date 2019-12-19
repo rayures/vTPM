e614cf408046c123e5dce6db0a97ac6d3b3c4aa2
rayures <rayures@users.noreply.github.com>
2019-12-19 16:27:24 +0100

    Update ubuntu19.10.sh

--- ubuntu19.10.sh
+++ ubuntu19.10.sh
@@ -13,7 +13,7 @@ read -rsp $'Press any key to continue...\n' -n1 key
 
 # Install dependent packages 
 apt-get update -y
-apt-get -y install automake expect gnutls-bin libgnutls28-dev git gawk m4 socat fuse libfuse-dev tpm-tools libgmp-dev libtool libglib2.0-dev libnspr4-dev libnss3-dev libssl-dev libtasn1-6-dev net-tools libseccomp-dev
+apt-get -y install automake expect gnutls-bin libgnutls28-dev git gawk m4 socat fuse libfuse-dev checkinstall tpm-tools libgmp-dev libtool libglib2.0-dev libnspr4-dev libnss3-dev libssl-dev libtasn1-6-dev net-tools libseccomp-dev
 
 read -rsp $'Press any key to continue...\n' -n1 key
 
@@ -24,9 +24,17 @@ git clone https://github.com/stefanberger/libtpms.git /home/vtpm/libtpms
 cd /home/vtpm/libtpms
 ./bootstrap.sh
 ./configure  --with-openssl --with-tpm2 &&                     # support TPM2.0
+read -rsp $'Press any key to continue...\n' -n1 key
 make &&
+#install on current system
+read -rsp $'Press any key to continue...\n' -n1 key
 make install                                               # optional
-
+#create .deb installation package
+read -rsp $'Press any key to continue...\n' -n1 key
+checkinstall
+#compress all files
+read -rsp $'Press any key to continue...\n' -n1 key
+tar -zcvf /home/vtpm/libtpms.tar.gz /home/vtpm/libtpms
 echo "libtpm finished"
 read -rsp $'Press any key to continue...\n' -n1 key
 
@@ -34,17 +42,19 @@ read -rsp $'Press any key to continue...\n' -n1 key
 git clone https://github.com/stefanberger/swtpm.git /home/vtpm/swtpm
 cd /home/vtpm/swtpm
 ./autogen.sh &&
+read -rsp $'Press any key to continue...\n' -n1 key
 ./configure  --prefix=/usr --with-openssl --with-tpm2 &&
+read -rsp $'Press any key to continue...\n' -n1 key
 make &&
+read -rsp $'Press any key to continue...\n' -n1 key
 make install                                             #optional
+read -rsp $'Press any key to continue...\n' -n1 key
+checkinstall
+read -rsp $'Press any key to continue...\n' -n1 key
+tar -zcvf /home/vtpm/swtpm.tar.gz /home/vtpm/swtpm
 
 echo "done with swtpm"
 cd /home/vtpm
 read -rsp $'Press any key to continue...\n' -n1 key
-
-#archiving folders
-tar -zcvf /home/vtpm/libtpms.tar.gz /home/vtpm/libtpms
-read -rsp $'Press any key to continue...\n' -n1 key
-tar -zcvf /home/vtpm/swtpm.tar.gz /home/vtpm/swtpm
 
 echo "finished"
